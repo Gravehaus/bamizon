748bde7edfbf3370a6792e4996818d6ac9762fb4
juliagraves <gravehaus@gmail.com>
2019-10-04 12:01:27 -0700

    REDID ENTIRE ASSIGNMENT

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,125 +1,116 @@
-const mysql = require('mysql');
-const Table = require('cli-table');
-const inquirer = require('inquirer');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
 
-//USE npm install cli-table FOR NODE ACCESS
-//MORE INFORMATION ON CLI-TABLES: https://www.npmjs.com/package/cli-table
-
-const displayTable = require('./constructorDisplay.js');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
+  // Your port; if not 3306
   port: 3306,
-  user: 'root',
-  password: 'password', //your mysql workbench password goes here
-  database:  'bamazon'
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "bamazon"
 });
 
-connection.connect(function (err) {
-  if (err) {
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
 
-    console.log('Error connecting to Db');
-    throw err;
-  }
-  displayForUser()
-})
-
-// Display products database using a table made with the npm package cli-table2
-// then Prompt the user to determine item and quantity they want to purchase
-//const displayForUser = function() {
-  function displayForUser(){
-  //const display = new displayTable();
-  connection.query('SELECT * FROM products', function(err, results){
-    //displayTable().displayInventoryTable(results);
-    console.table(results);
-   purchaseItem(results);
-    // customerPrompt();
-  });
-}
-
-// TELLS THE USER TO PICK SOMETHING BY ITEM ID
-// function purchaseItem(){
-//const purchaseItem = function() {
-function purchaseItem(inventoryArray){
-  console.log('\n  ');
-  inquirer.prompt([{
-    name: "id",
-    type: "input",
-    message: " Enter the Item ID of the product you want to purchase",
-
-  }, {
-    name: "quantity",
-    type: "input",
-    message: " Enter the quantity you want to purchase",
-
-  }]).then(function(answer) {
-    // ASKS THE DB IF SOMETHING IS IN STOCK, AND HOW MANY.
-    // connection.query('SELECT ProductName, DepartmentName, Price, StockQuantity FROM products WHERE ?', {ItemID: parseInt(answer.id)}, function(err,res) {
-
-    let  selectedItem;
-    for (i=0; i<inventoryArray.length;i++){
-      // console.log("inventory:this is a an ID ", inventoryArray[i].ItemID );
-      // console.log("answer:this is a an ID ", answer.id );
-      if(inventoryArray[i].ItemID  === parseInt(answer.id)){
-
-
-        selectedItem=inventoryArray[i];
-        // console.log("this is a valid ID Order");
-        if(selectedItem.StockQuantity >= answer.quantity){
-          //this is going to tell the remaining quantity GREAT BAY UPDATE QUERY-REFERENCE//
-
-          console.log("this is an appropriate order");
-
+// function which prompts the user for what action they should take
+function start() {
+  connection.query("SELECT * FROM products",  function(err, response){
+    if (err) throw err;
+    console.table(response);
+    inquirer
+      .prompt({
+        name: "welcome",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["Purchase", "EXIT"]
+      })
+      .then(function(answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.welcome === "Purchase") {
+          buyItem();
+        } else{
+          connection.end();
         }
-      }
-      console.log(inventoryArray[i]);
-    }
-      console.log('\n  You would like to buy ' + answer.quantity + ' ' + res[0].ProductName + ' ' + res[0].DepartmentName + ' at $' + res[0].Price + ' each'
-      );
-      if (res[0].StockQuantity >= answer.quantity) {
-        const itemQuantity = res[0].StockQuantity - answer.quantity;
-        connection.query("UPDATE products SET ? WHERE ?", [
-          {
-            StockQuantity: itemQuantity
-          }, {
-            ItemID: answer.id
-          }], function(err,res) {
-        });
-        const cost = res[0].Price * answer.quantity;
-        console.log('\n  Order fulfilled! Your cost is $' + cost.toFixed(2) + '\n');
-        // Order completed
-        customerPrompt();
+      });
 
-      } else {
-        //IF ITEM IS UNAVAILABLE, THIS MESSAGE GENERATES
-        console.log('\n  Sorry, Insufficient quantity to fulfill your order!\n');
-        // ORDER CANNOT BE COMPLETED.
-        customerPrompt();
-      }
-    // })
+  })
+}
+
+function buyItem() {
+  // query the database
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+    //
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].ProductName);
+            }
+            return choiceArray;
+          },
+          message: "What item do you want to buy? (enter the ID# or when selected, press enter)"
+        },
+        {
+          name: "stock",
+          type: "number",
+          message: "How many would you like to buy?"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].ProductName === answer.choice) {
+            chosenItem = results[i];
+          }
+        }
+
+        // determine if stock is enough
+        if (chosenItem.StockQuantity < parseInt(answer.StockQuantity)) {
+          // bid was high enough, so update db, let the user know, and start over
+          connection.query(
+            "UPDATE products SET StockQuantity = StockQuantity -  ? WHERE ?",
+            [
+              answer.StockQuantity,
+              {
+                id: chosenItem.id
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("Item has been added to cart.");
+              console.log("Thank you for your purchase! Your total is " + "$" + parseInt(answer.StockQuantity) * chosenItem.Price);
+              start();
+            }
+          );
+        }
+        else {
+          // 86'ed Item
+          console.log("Currently Not Available");
+          start();
+        }
+      });
   });
 }
 
-//const customerPrompt = function() {
-function customerPrompt(){
-  inquirer.prompt({
-    name: "action",
-    type: "list",
 
-    message: " Would like to continue shopping?\n",
-    choices: ["Yes", "No"]
-  }).then(function(answer) {
-    switch(answer.action) {
-      case 'Yes':
-        displayForUser();
-        break;
 
-      case 'No':
-        connection.end();
-        break;
-    }
-  })
-};
 
-// Start app by Prompting the customer
+
+
--- constructorDisplay.js
+++ constructorDisplay.js
@@ -1,27 +1,110 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-//INSTALL CLI-TABLE2 TO AUTO GENERATE A TABLE FOR USER.
-//USE npm install cli-table2 FOR TABLE GENERATION
-//MORE INFORMATION ON CLI-TABLES: https://www.npmjs.com/package/cli-table2
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
 
-// const port = process.env.PORT || 3306 ;
-// console.log('Server is running on port: ' + port);
+  // Your port; if not 3306
+  port: 3306,
 
-Table = require('cli-table');
-const displayTable = function() {
+  // Your username
+  user: "root",
 
-  this.table = new Table({
-    head: ['Item ID', 'Product Name', 'Price', 'Stock Quantity'],
-  });
+  // Your password
+  password: "password",
+  database: "bamazon"
+});
 
-  this.displayInventoryTable = function(results) {
-    this.results = results;
-    for (let i=0; i <this.results.length; i++) {
-      this.table.push(
-        [this.results[i].ItemID, this.results[i].ProductName, '$'+ this.results[i].Price, this.results[i].StockQuantity] );
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
 
-      //WHY IS THIS NOT WORKING///
-    }
-    console.log('\n' + this.table.toString());
-  };
+// function which prompts the user for what action they should take
+function start() {
+  connection.query("SELECT * FROM products",  function(err, response){
+    if (err) throw err;
+    console.table(response);
+    inquirer
+      .prompt({
+        name: "welcome",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["BUY SOMETHING", "EXIT"]
+      })
+      .then(function(answer) {
+        // based on their answer, either call the bid or the post functions
+        if (answer.welcome === "BUY SOMETHING") {
+          buyItem();
+        } else{
+          connection.end();
+        }
+      });
+
+  })
 }
-module.exports = displayTable;
+
+function buyItem() {
+  // query the database 
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+    // 
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].product_name);
+            }
+            return choiceArray;
+          },
+          message: "What item do you want to buy? (enter the ID# or when selected, press enter)"
+        },
+        {
+          name: "stock",
+          type: "number",
+          message: "How many would you like to buy?"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].product_name === answer.choice) {
+            chosenItem = results[i];
+          }
+        }
+
+        // determine if stock is enough
+        if (chosenItem.stock_quantity > parseInt(answer.stock)) {
+          // bid was high enough, so update db, let the user know, and start over
+          connection.query(
+            "UPDATE products SET stock_quantity = stock_quantity -  ? WHERE ?",
+            [
+              answer.stock,
+              {
+                id: chosenItem.id
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("We have enough in stock!");
+              console.log("Thank you for your business! Your total is " + "$" + parseInt(answer.stock) * chosenItem.price);
+              start();
+            }
+          );
+        }
+        else {
+          // not enough stock
+          console.log("Not enough in stock");
+          start();
+        }
+      });
+  });
+}
